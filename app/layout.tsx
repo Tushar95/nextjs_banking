8b6a43420965a81f2b25748e08cfc78d04b587ba
codewiz <tushareze@gmail.com>
2025-07-08 22:22:37 +0530

    indented layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,21 +3,22 @@ import "./globals.css";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
 
 const inter = Inter({
-  subsets: ["latin"], variable: "--font-inter"
+  subsets: ["latin"],
+  variable: "--font-inter",
 });
 
 const ibmPlexSerif = IBM_Plex_Serif({
   subsets: ["latin"],
-  weight: ['400', '700'],
-  variable: "--font-ibm-plex-serif"
+  weight: ["400", "700"],
+  variable: "--font-ibm-plex-serif",
 });
 
 export const metadata: Metadata = {
   title: "Home Bank",
   description: "Modern banking platform",
   icons: {
-    icon: '/icons/logo.svg'
-  }
+    icon: "/icons/logo.svg",
+  },
 };
 
 export default function RootLayout({
