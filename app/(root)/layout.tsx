9311ab6a81e800bb80d8d41bad4f4569a6e56494
codewiz <tushareze@gmail.com>
2025-07-08 22:14:40 +0530

    First banking app commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import "@/app/globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+      SIDEBAR
+      {children}
+    </main>
+  );
+}
